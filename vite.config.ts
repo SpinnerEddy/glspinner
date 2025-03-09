2d6026605192883b63c157ae6df60790dc692bab
独楽回しeddy <kiryu1022@gmail.com>
2025-03-09 16:16:10 +0900

    パス周りの整理

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
 import { defineConfig } from "vite";
-import glsl from 'vite-plugin-glsl'
+import glsl from 'vite-plugin-glsl';
+import path from 'path';
 
 export default defineConfig({
+    root: 'examples',
+    base: '/',
     build: {
+        outDir: path.resolve(__dirname, 'dist'),
+        emptyOutDir: true,
         lib: {
-            entry: './src/index.ts',
+            entry: path.resolve(__dirname, 'src/index.ts'),
             name: 'spinnergl',
-            fileName: 'spinnergl-lib',
-        }
+            fileName: (format) => `spinnergl-lib.${format}.js`,
+            formats: ['es', 'cjs', 'umd']
+        },
     },
     server: {
         port: 2222
+    },
+    resolve: {
+        alias: {
+            '@webgl': path.resolve(__dirname, 'src/webgl')
+        }
     },
     plugins: [
         glsl({
