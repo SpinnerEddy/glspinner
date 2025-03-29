49168ffbe1be57621d40f381307724c6f57afe30
独楽回しeddy <kiryu1022@gmail.com>
2025-03-29 20:00:27 +0900

    indexバッファの扱い周りの対応

---
+++ src/webgl/gl/buffer/IndexBuffer.ts
@@ -0,0 +1,34 @@
+import { BaseBuffer } from "./BaseBuffer";
+
+export class IndexBuffer extends BaseBuffer{
+    private indices: Int16Array;
+
+    constructor(gl: WebGL2RenderingContext, indices: Int16Array){
+        super(gl);
+        this.indices = indices;
+    }
+
+    get BufferType(): number {
+        return this.gl.ARRAY_BUFFER;
+    }
+
+    bind(): void {
+        this.gl.bindBuffer(this.BufferType, this.buffer);
+    }
+
+    unbind(): void {
+        this.gl.bindBuffer(this.BufferType, null);
+    }
+
+    setData(): void {
+        this.gl.bindBuffer(this.BufferType, this.buffer);
+        this.gl.bufferData(this.BufferType, this.indices, this.gl.DYNAMIC_DRAW);
+    }
+
+    dispose(): void {
+        if (this.buffer){
+            this.gl.deleteBuffer(this.buffer);
+            this.buffer = null;
+        }
+    }
+}
