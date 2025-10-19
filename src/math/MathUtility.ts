35762afb9a926fc3806d76699af5068948e535d5
SpinnerEddy <kiryu1022@gmail.com>
2025-10-19 21:52:15 +0900

    BlurMaterialを追加

--- src/math/MathUtility.ts
+++ src/math/MathUtility.ts
@@ -17,32 +17,57 @@ export class MathUtility{
         return Math.max(Math.min(inputValue, 1), 0);
     }
 
-    static sin(angle: number): number{
+    static sin(angle: number): number {
         const value = Math.sin(angle);
         return MathUtility.roundToZero(value);
     }
 
-    static cos(angle: number): number{
+    static cos(angle: number): number {
         const value = Math.cos(angle);
         return MathUtility.roundToZero(value);
     }
 
-    static tan(angle: number): number{
+    static tan(angle: number): number {
         const value = Math.tan(angle);
         return MathUtility.roundToZero(value);
     }
 
-    static acos(angle: number): number{
+    static exp(value: number): number {
+        const result = Math.exp(value);
+        return MathUtility.roundToZero(result);
+    }
+
+    static acos(angle: number): number {
         const value = Math.acos(angle);
         return MathUtility.roundToZero(value);
     }
 
-    static atan2(y: number, x: number): number{
+    static atan2(y: number, x: number): number {
         const value = Math.atan2(y, x);
         return MathUtility.roundToZero(value);
     }
+
+    static calculateGaussianCoefficients(range: number, count: number): Float32Array {
+        let gCoefficients: number[] = [];
+        let totalRange = range * 2.0;
+        let startRange = -range;
+        let incrementValue = totalRange / count;
+        let totalValue = 0.0;
+
+        for(let i = startRange; i <= range; i += incrementValue){
+            let value = MathUtility.exp(-(i * i) / totalRange);
+            totalValue += value;
+            gCoefficients.push(value);
+        }
+
+        for(let i = 0; i < gCoefficients.length; i++){
+            gCoefficients[i] /= totalValue;
+        }
+
+        return new Float32Array(gCoefficients);
+    }
     
-    private static roundToZero(inputValue: number){
+    private static roundToZero(inputValue: number) {
         return Math.abs(inputValue) < DefaultValueConstants.EPSILON ? 0 : inputValue;
     }
 }
