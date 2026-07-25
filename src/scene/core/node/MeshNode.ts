b48534997522e3d070d5ac801c4b0287b764f924
SpinnerEddy <kiryu1022@gmail.com>
2026-07-25 20:26:22 +0900

    不要な関数を削除

--- src/scene/core/node/MeshNode.ts
+++ src/scene/core/node/MeshNode.ts
@@ -1,4 +1,3 @@
-import { ShaderUniformValue } from '../../../webgl/gl/uniform/ShaderUniformValue';
 import { BaseMesh } from '../../mesh/BaseMesh';
 import { RenderTagConstants } from '../../renderer/definition/RenderTag';
 import { RendererContext } from '../../renderer/RendererContext';
@@ -19,15 +18,7 @@ export class MeshNode extends SceneNode {
 
     public draw(gl: WebGL2RenderingContext, context: RendererContext): void {
         this.mesh.useMaterial(gl, context);
-        this.updateUniforms(gl, context);
-        this.mesh.draw(gl);
-    }
-
-    private updateUniforms(gl: WebGL2RenderingContext, context: RendererContext): void {
-        const modelMatrix = this.transform.getWorldMatrix();
-
-        context.updateGlobalUniform('modelMatrix', new ShaderUniformValue(modelMatrix));
-
         this.mesh.updateUniforms(gl, context, this.transform);
+        this.mesh.draw(gl);
     }
 }
